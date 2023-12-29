e4b2e8487df35cd3519c92e39303638d7481b78d
Raul Madero <r.madero.ramirez@gmail.com>
2023-12-29 17:05:06 -0600

    Modificacion en la pagina team

--- views/paginas/equipo.php
+++ views/paginas/equipo.php
@@ -5,15 +5,6 @@
           <h2>Conocenos</h2>
           <P>Somos un equipo especializado con años de experiencia</P>
         </div>
-      </section>
-      <section class="contenedor equipo-card-container">
-        <?php foreach($colaboradores as $colaborador) : ?>
-        <div class="equipo-card">
-          <img class="equipo-card__image" src="./img/alex.webp" alt="Alejandro">
-          <p class="equipo-card__name"><?php echo $colaborador->nombre; ?></p>
-          <p class="equipo-card__post"><?php echo $colaborador->apellido; ?></p>
-        </div>
-        <?php endforeach; ?>
-        </section>   
+      </section> 
     <a href="/nosotros" class="boton-verde">Volver</a>
     </main>
